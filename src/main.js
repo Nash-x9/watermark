5037f946ae4bf43ec0945f6753d9bd2de90c8951
Nash-x9 <704005406@qq.com>
2023-11-16 18:18:27 +0800

    [+] 引入颜色选择组件

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import './assets/main.css'
 
-import { createApp } from 'vue'
+import {createApp} from 'vue'
 import App from './App.vue'
-
-createApp(App).mount('#app')
+import ColorPicker from 'colorpicker-v3'  // 注册组件
+import 'colorpicker-v3/style.css' // 引入样式文件
+const app = createApp(App)
+app.use(ColorPicker)
+app.mount('#app')
